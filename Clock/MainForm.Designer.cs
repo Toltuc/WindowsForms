3323e8eddfa4befc8508fb16c2508b7104be4640
Toltuc <pikimi305@gmail.com>
2026-03-05 19:35:47 +0800

    Add project files.

---
+++ Clock/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Clock
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.labelTime = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.cbShowDate = new System.Windows.Forms.CheckBox();
+            this.cbShowWeekday = new System.Windows.Forms.CheckBox();
+            this.btnHideControls = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.BackColor = System.Drawing.SystemColors.Highlight;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(17, 16);
+            this.labelTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(324, 63);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "Current time";
+            this.labelTime.DoubleClick += new System.EventHandler(this.labelTime_DoubleClick);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // cbShowDate
+            // 
+            this.cbShowDate.AutoSize = true;
+            this.cbShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbShowDate.Location = new System.Drawing.Point(29, 220);
+            this.cbShowDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbShowDate.Name = "cbShowDate";
+            this.cbShowDate.Size = new System.Drawing.Size(164, 35);
+            this.cbShowDate.TabIndex = 1;
+            this.cbShowDate.Text = "Show date";
+            this.cbShowDate.UseVisualStyleBackColor = true;
+            // 
+            // cbShowWeekday
+            // 
+            this.cbShowWeekday.AutoSize = true;
+            this.cbShowWeekday.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbShowWeekday.Location = new System.Drawing.Point(29, 265);
+            this.cbShowWeekday.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbShowWeekday.Name = "cbShowWeekday";
+            this.cbShowWeekday.Size = new System.Drawing.Size(219, 35);
+            this.cbShowWeekday.TabIndex = 2;
+            this.cbShowWeekday.Text = "Show weekday";
+            this.cbShowWeekday.UseVisualStyleBackColor = true;
+            // 
+            // btnHideControls
+            // 
+            this.btnHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnHideControls.Location = new System.Drawing.Point(29, 309);
+            this.btnHideControls.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnHideControls.Name = "btnHideControls";
+            this.btnHideControls.Size = new System.Drawing.Size(320, 66);
+            this.btnHideControls.TabIndex = 3;
+            this.btnHideControls.Text = "Hide controls";
+            this.btnHideControls.UseVisualStyleBackColor = true;
+            this.btnHideControls.Click += new System.EventHandler(this.btnHideControls_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 390);
+            this.Controls.Add(this.btnHideControls);
+            this.Controls.Add(this.cbShowWeekday);
+            this.Controls.Add(this.cbShowDate);
+            this.Controls.Add(this.labelTime);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Clock P_421";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label labelTime;
+		private System.Windows.Forms.Timer timer;
+		private System.Windows.Forms.CheckBox cbShowDate;
+		private System.Windows.Forms.CheckBox cbShowWeekday;
+		private System.Windows.Forms.Button btnHideControls;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
